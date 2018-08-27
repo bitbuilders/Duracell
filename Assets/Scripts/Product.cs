8ac09cb95bef9871e0b847c9cf1ac71faab005bf
Colin Misbach <colin@misbach.com>
2018-08-27 12:51:58 -0600

    Added art assets | Started on spawner

--- Assets/Scripts/Product.cs
+++ Assets/Scripts/Product.cs
@@ -15,26 +15,59 @@ public class Product : MonoBehaviour
     [SerializeField] Sprite m_flashlightPNG = null;
     [SerializeField] Sprite m_toasterPNG = null;
     [SerializeField] Sprite m_robotPNG = null;
+    [SerializeField] [Range(0.0f, 10.0f)] float m_falloffRate = 1.0f;
     [SerializeField] ProductType m_type = ProductType.FLASHLIGHT;
 
+    Rigidbody2D m_rigidbody2D;
     SpriteRenderer m_spriteRenderer;
     Vector3 m_velocity;
     
     void Start()
     {
         m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
     }
     
     void Update()
     {
-        transform.position += m_velocity;
+
     }
 
     public void Initialize(Vector3 velocity, ProductType type = ProductType.FLASHLIGHT)
     {
         m_velocity = velocity;
-
+        if (m_rigidbody2D == null)
+            m_rigidbody2D = GetComponent<Rigidbody2D>();
+        m_rigidbody2D.AddForce(m_velocity, ForceMode2D.Impulse);
+        
+        if (m_spriteRenderer == null)
+            m_spriteRenderer = GetComponent<SpriteRenderer>();
         m_type = type;
+        switch (m_type)
+        {
+            case ProductType.FLASHLIGHT:
+                m_spriteRenderer.sprite = m_flashlightPNG;
+                break;
+            case ProductType.ROBOT:
+                m_spriteRenderer.sprite = m_robotPNG;
+                break;
+            case ProductType.TOASTER:
+                m_spriteRenderer.sprite = m_toasterPNG;
+                break;
+        }
+    }
+
+    public void InitializeRandom(Vector3 velocity)
+    {
+        m_velocity = velocity;
+        if (m_rigidbody2D == null)
+            m_rigidbody2D = GetComponent<Rigidbody2D>();
+        m_rigidbody2D.AddForce(m_velocity, ForceMode2D.Impulse);
+
+        if (m_spriteRenderer == null)
+            m_spriteRenderer = GetComponent<SpriteRenderer>();
+        int x = Random.Range(0, 3);
+        m_type = (ProductType)x;
         switch (m_type)
         {
             case ProductType.FLASHLIGHT:
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,15 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour {
+public class Spawner : MonoBehaviour
+{
+    [System.Serializable]
+    public enum Difficulty
+    {
+        EASY,
+        MEDIUM,
+        HARD
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] GameObject m_spawnee = null;
+    [SerializeField] [Range(0.0f, 5.0f)] float m_spawnRate = 2.0f;
+    [SerializeField] GameObject m_spawnPointMIN = null;
+    [SerializeField] GameObject m_spawnPointMAX = null;
+    [SerializeField] [Range(0.0f, 90.0f)] float m_startAngleMIN = 15.0f;
+    [SerializeField] [Range(0.0f, 90.0f)] float m_startAngleMAX = 35.0f;
+    [SerializeField] [Range(0.0f, 10.0f)] float m_startForceMIN = 1.0f;
+    [SerializeField] [Range(0.0f, 10.0f)] float m_startForceMAX = 2.0f;
+    [SerializeField] Difficulty m_difficulty = Difficulty.EASY;
+
+    List<float> m_difficultyRates = new List<float>() { 0.05f, 0.10f, 0.15f };
+    List<int> m_difficultyCounts = new List<int>() { 10, 20, 30 };
+
+    float m_currentSpawnRate;
+    float m_time;
+    int m_count;
+
+    void Start()
+    {
+        int diff = (int)m_difficulty;
+        m_currentSpawnRate = m_spawnRate;
+        m_count = m_difficultyCounts[diff];
+    }
+    
+    void Update()
+    {
+        m_time += Time.deltaTime;
+        if (m_time >= m_currentSpawnRate)
+        {
+            m_time = 0.0f;
+            SpawnProduct();
+        }
+
+        m_currentSpawnRate -= Time.deltaTime * m_difficultyRates[(int)m_difficulty];
+    }
+
+    public void SpawnProduct()
+    {
+        GameObject go = Instantiate(m_spawnee, GetRandomPoint(), Quaternion.identity, transform);
+        Product product = go.GetComponent<Product>();
+        Vector3 velocity = Vector3.left;
+        float force = Random.Range(m_startForceMIN, m_startForceMAX);
+        velocity *= force;
+        float angle = Random.Range(m_startAngleMIN, m_startAngleMAX);
+        velocity = Quaternion.AngleAxis(-angle, Vector3.forward) * velocity;
+        product.InitializeRandom(velocity);
+    }
+
+    public Vector2 GetRandomPoint()
+    {
+        Vector2 point = Vector2.zero;
+
+        float y = Random.Range(m_spawnPointMIN.transform.position.y, m_spawnPointMAX.transform.position.y);
+        float x = m_spawnPointMAX.transform.position.x;
+        point = new Vector2(x, y);
+
+        return point;
+    }
 }
